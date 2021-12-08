8dd2d193fb52794770043296eeb85c6fb4464229
alan birrueta <alanb_99@hotmail.com>
2021-12-08 15:00:21 -0700

    adding a function to play one round vs the computer

--- script.js
+++ script.js
@@ -8,4 +8,41 @@ function computerPlay() {
     return "Scissors";
   }
 }
-console.log(computerPlay());
+
+function playRound(playerSelection, computerSelection) {
+  let player = playerSelection.toUpperCase();
+  let computer = computerSelection.toUpperCase();
+  let youWin = "You Win!";
+  let youLose = "You Lose!";
+
+  let rockWins = "Rock beats Scissors";
+  let paperWins = "Paper beats Rock";
+  let scissorsWins = "Scissors beats Paper";
+
+  if (player === computer) {
+    return "tie";
+    // if the player wins
+  } else if (player === "ROCK" && computer === "SCISSORS") {
+    return `${youWin} ${rockWins}`;
+  } else if (player === "PAPER" && computer === "ROCK") {
+    return `${youWin} ${paperWins}`;
+  } else if (player === "SCISSORS" && computer === "PAPER") {
+    return `${youWin} ${scissorsWins}`;
+  }
+  //   if the computer wins
+  else if (computer === "ROCK" && player === "SCISSORS") {
+    return `${youLose} ${rockWins}`;
+  } else if (computer === "PAPER" && player === "ROCK") {
+    return `${youLose} ${paperWins}`;
+  } else if (computer === "SCISSORS" && player === "PAPER") {
+    return `${youLose} ${scissorsWins}`;
+  }
+}
+
+let playerSelection = "scissors";
+console.log(playerSelection);
+let computerSelection = computerPlay();
+console.log(computerSelection);
+
+console.log(playRound(playerSelection, computerSelection));
+// let userInput = prompt("Select a weapon, rock paper or scissors");
